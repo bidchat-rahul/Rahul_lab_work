789a13fd3fbb641a5150fc8b49ebad96109eb3c7
Rahul harmalkar <rharmalkar28@hotmail.com>
2016-01-27 12:01:44 +0530

    27 jan

---
+++ pairTest/src/pairTest/constrctorTest.java
@@ -0,0 +1,16 @@
+package pairTest;
+
+public class constrctorTest {
+ int a ;
+ String b;
+ 	public constrctorTest(int a) {
+		this.a = a;
+	}
+
+
+
+	public constrctorTest(int a, String b) {
+		this.a = a;
+		this.b = b;
+	}
+}
